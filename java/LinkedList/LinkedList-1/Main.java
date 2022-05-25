390c318ba666bb37ee83bb64f444ca413a78e28e
rishavkr07 <rishav0797@gmail.com>
2022-05-25 16:30:19 +0530

    intial commit

--- java/LinkedList/LinkedList-1/Main.java
+++ java/LinkedList/LinkedList-1/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static class Person{
         String name;
         int age;
+        int contact;
     }
 public static void main(String[] args){
     Person p1 = new Person();
@@ -9,7 +10,8 @@ public static void main(String[] args){
      
      p1.name="admin";
      p1.age=20;
+     p1.contact=9876543210;
 
-     System.out.println(p1.name+" "+p1.age);
-}
+     System.out.println(p1.name+" "+p1.age+" "+p1.contact);
+    }
 }
