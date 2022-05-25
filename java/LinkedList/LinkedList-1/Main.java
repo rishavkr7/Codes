a4f6def6457e0edc712e60813d213d413f37011a
rishavkr07 <rishav0797@gmail.com>
2022-05-25 16:45:46 +0530

    intaial commit

--- java/LinkedList/LinkedList-1/Main.java
+++ java/LinkedList/LinkedList-1/Main.java
@@ -2,15 +2,15 @@ public class Main {
     public static class Person{
         String name;
         int age;
-        int contact;
+        String contact;
     }
 public static void main(String[] args){
     Person p1 = new Person();
-     System.out.println(p1.name+" "+p1.age);
+     System.out.println(p1.name+" "+p1.age);  //By Default String is null and Integer is 0
      
      p1.name="admin";
      p1.age=20;
-     p1.contact=9876543210;
+     p1.contact="8809892355";
 
      System.out.println(p1.name+" "+p1.age+" "+p1.contact);
     }
